3fdea2873d2b747504b7f16e42e44b4ea94211a0
arya271407 <arya271407@gmail.com>
2025-11-15 09:21:06 +0530

    modified math.py program

--- math.py
+++ math.py
@@ -2,7 +2,12 @@ try:
   a = float(input("Enter first number"))
   b = float(input("Enter second number"))
 
-print(f"Addition: {a+b}")
-print(f"Subtraction: {a-b}")
+  print(f"Addition: {a+b}")
+  print(f"Subtraction: {a-b}")
+  print(f"Multiplication: {a*b}")
+  if b != 0:
+    print(f"Division: {a/b}")
+  else:
+    print("Division: Cannot divide by zero.")
 except ValueError:
-print("Please enter valid numbers.")
+  print("Please enter valid numbers.")
